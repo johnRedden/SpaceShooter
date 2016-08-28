62c237b9b9234f69ede5ee721e976b1815909577
John Redden <jtredden@gmail.com>
2016-08-28 12:42:35 -0700

    init Space Shooter
    Start of project

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject hazard;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+    public GUIText scoreText;
+    public GUIText restartText;
+    public GUIText gameOverText;
+
+    private int score;
+    private bool restart;
+    private bool gameOver;
+    
+	// Use this for initialization
+	void Start () {
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
+
+        StartCoroutine(SpawnWaves());
+        score = 0;
+        updateScore();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }
+	
+	}
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+            if (gameOver)
+            {
+                restartText.text = "Press 'R' for Restart.";
+                restart = true;
+                break;
+            }
+
+        }
+
+
+    }
+    public void addScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        updateScore();
+    }
+    void updateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+    public void GameOver()
+    {
+        gameOverText.text = "Game Over!";
+        gameOver = true;
+
+    }
+
+
+
+
+}
